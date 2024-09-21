ac11fe4db3bef8b71b6472c2bddd3a250118f286
abitha0020 <132005925+abitha0020@users.noreply.github.com>
2024-09-21 23:18:14 +0530

    Update App.tsx

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,12 +1,12 @@
 import './App.css'
+import Navigator from './Routes/index'
+import { BrowserRouter } from "react-router-dom";
 function App() {
 
   return (
-    <>
-      <h1 className="text-3xl font-bold text-center">
-        Hello world!
-      </h1>
-    </>
+    <BrowserRouter>
+      <Navigator />
+    </BrowserRouter>
   )
 }
 

57765f2e4bb1af61aa6b3e349d8e40e2951a8572
abitha0020 <132005925+abitha0020@users.noreply.github.com>
2024-09-21 11:02:05 +0530

    Connecting Firebase

--- Backend/index.js
+++ Backend/index.js
@@ -1,13 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
-// Define a route handler for the root path
+app.use(cors()); 
+
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send('Hello World');
 });
 
-// Start the server on port 3000
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });

ce227404424727f759c4d09b98dd6bd6dd63eb49
abitha0020 <132005925+abitha0020@users.noreply.github.com>
2024-09-21 22:19:43 +0530

    Update Ownership.tsx

--- Frontend/src/Pages/Ownership.tsx
+++ Frontend/src/Pages/Ownership.tsx
@@ -79,7 +79,7 @@ export default function Ownership() {
           </svg>
         </div>
         <div className="w-1/3 mx-auto h-[28rem] bg-white p-8 border border-gray-200 rounded-xl shadow-lg w-[40%]">
-        <h2 className="text-2xl text-center font-bold mb-6 text-gray-800">Current Owner</h2>
+        <h2 className="text-2xl text-center font-bold mb-6 text-gray-800">New Owner</h2>
         <form>
           <div className="mb-4">
             <label htmlFor="name" className="block text-gray font-medium mb-2">

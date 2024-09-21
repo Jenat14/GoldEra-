5d3e29fbdfb98ef512e46c68ca57b872cb7a0e50
unknown <jenatcanil14@gmail.com>
2024-09-21 21:52:32 +0530

    Updated button

--- Frontend/src/Components/button.tsx
+++ Frontend/src/Components/button.tsx
@@ -5,11 +5,10 @@ interface buttontitle{
 export default function Button({title,onClick}:buttontitle){
     return(
         <div className="h-14 w-[200px] md:w-[300px] lg:w-[360px] mx-auto ">
-             <button className="h-14 w-[200px] md:w-[300px] lg:w-[360px] rounded-full bg-[#3F2305] text-white font-medium text-2xl"
+             <button className="h-14 w-[200px] md:w-[300px] lg:w-[360px] rounded-full bg-[#3F2305] text-white font-medium text-2xl hover:bg-[#C07F00]"
              onClick={onClick}>
                 {title}
             </button>
         </div>
-        
     );
 }
